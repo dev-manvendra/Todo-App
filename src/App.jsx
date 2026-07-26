f2c27c555a0091f2f902c862373124949e7ae1f1
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-07-26 11:00:44 +0530

    create a component TodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
+import { TodoContextProvider } from './context/TodoContext'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <TodoContextProvider>
       <h1 className="bg-amber-800 text-white text-xl p-1 ">this is the heading</h1>
-    </>
+    </TodoContextProvider>
   )
 }
 
